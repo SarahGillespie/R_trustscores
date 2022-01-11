2aa5f2ea3d23f2cb55a606090e73dcd671c669c9
Sarah Gillespie <sarahelizabethgillespie@gmail.com>
2022-01-10 21:38:24 -0500

    update the R file. make it teachable next.

--- trustscore_annotated.py
+++ trustscore_annotated.py
@@ -1,23 +1,23 @@
 ###############################################################################
 # Copyright 2018 Google LLC                                                   #
-#
+#                                                                             #
 # Licensed under the Apache License, Version 2.0 (the "License");             #
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# you may not use this file except in compliance with the License.            #
+# You may obtain a copy of the License at                                     #
+#                                                                             #
+#     https://www.apache.org/licenses/LICENSE-2.0                             #
+#                                                                             #
+# Unless required by applicable law or agreed to in writing, software         #
+# distributed under the License is distributed on an "AS IS" BASIS,           #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied     #
+# See the License for the specific language governing permissions and         #
+# limitations under the License.                                              #
 ###############################################################################
-# Additional explanatory comments added by Sarah Gillespie
-# https://github.com/SarahGillespie
-# 7 Jan 2022
-#
-# Most comments are in the fit() function.
+# Additional explanatory comments added by Sarah Gillespie.                   #
+# https://github.com/SarahGillespie                                           #
+# 7 Jan 2022                                                                  #
+#                                                                             #
+# Most comments are regarding the fit() function.                             #
 ###############################################################################
 
 import numpy as np
@@ -90,6 +90,7 @@ class TrustScore:
     
     
     # this function to uses numpy arrays as its inputted data structure for both the x and y inputs.
+    # the outputted object is also a numpy array.
     #    - Sarah Gillespie
         
         self.n_labels = np.max(y) + 1
